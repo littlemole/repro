b3e26a42a594efa783b4d753d8cb1b9e59297b08
little.mole <little.mole@oha7.org>
2018-06-29 20:32:20 +0200

    proper overload for wrap_exception

--- include/reprocpp/ex.h
+++ include/reprocpp/ex.h
@@ -9,18 +9,8 @@
 #include <vector>
 #include <functional>
 
-/*
- * mainly provides promise base exception.
- * additionally there is Wrapped Exception base
- * to wrap an exception for asynchronous dispatch.
- * to test any exception derived from Ex, including
- * Wrapped Exceptions against a given type, use
- * the isa() helper template below.
- *
- */
 
 namespace repro {
-
 
 
 class Ex : public std::exception
@@ -50,33 +40,19 @@ public:
         
     std::string msg;
 };
-/*
-class WrappedException : public Ex
-{
-public:
-	WrappedException()
-	{}
 
-	virtual std::exception_ptr ptr() const = 0;
-	virtual void raise(const std::function<void(const std::exception&)> fun) const
-	 = 0;
-
-};
-*/
 class WrappedException : public Ex
 {
 public:
 
 	template<class E>
 	WrappedException(const E& e)
-	: e_(std::make_exception_ptr(e))
-	{
-	}
+		: e_(std::make_exception_ptr(e))
+	{}
 
 	WrappedException(const std::exception_ptr& e)
-	: e_(e)
-	{
-	}
+		: e_(e)
+	{}
 
 	virtual std::exception_ptr ptr() const
 	{
@@ -105,61 +81,32 @@ private:
 template<class E>
 WrappedException wrap_exception(const E& e)
 {
+	const WrappedException* p = dynamic_cast<const WrappedException*>(&e);
+	if(p)
+	{
+		return *p;
+	}
 	return WrappedException(e);
 }
-/*
-class WrappedEx : public Ex
+
+inline WrappedException wrap_exception(std::exception_ptr e)
 {
-public:
-	WrappedEx(const std::exception& ex)
-	:Ex(ex.what()),type_(typeid(ex))
-	{
-		const WrappedEx* we = dynamic_cast<const WrappedEx*>(&ex);
-		if (we)
-		{
-			type_ = we->type();
-		}
-	}
-
-	const std::type_index& type() const
-	{
-		return type_;
-	}
-
-protected:
-	std::type_index type_;
-};
+	return WrappedException(e);
+}
 
 namespace ex {
-
-inline bool isWrapped(const std::exception& ex)
-{
-	return dynamic_cast<const WrappedEx*>(&ex) != 0;
-};
-
 
 template<class T>
 bool isa(const std::exception& ex)
 {
-	if( dynamic_cast<const T*>(&ex) ) {
+	if( dynamic_cast<const T*>(&ex) ) 
+	{
 		return true;
 	}
-
-	if(isWrapped(ex))
-	{
-		const WrappedEx* wrapped = dynamic_cast<const WrappedEx*>(&ex);
-		if (wrapped->type() == std::type_index(typeid(T)) )
-		{
-			return true;
-		}
-	}
-
 	return false;
 }
 
-*/
-
-}
+}}
 
 #endif
 
